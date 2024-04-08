419f80aff7a3c8a2cbffefd923b23e48298caa52
AkashSDas <aakashdas368@gmail.com>
2024-04-08 06:59:06 +0530

    add clerk for auth and add auth in header component

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Duolingo
+
+## Technologies
+
+- React / NextJS (app router)
+- ShadCN UI / Tailwind CSS
+- Clerk
+
+- [Kenny for assets](https://kenney.nl/assets)
--- src/app/(marketing)/header.tsx
+++ src/app/(marketing)/header.tsx
@@ -1,7 +1,54 @@
+import {
+    ClerkLoading,
+    ClerkLoaded,
+    SignedIn,
+    UserButton,
+    SignedOut,
+    SignInButton,
+} from "@clerk/nextjs";
+import Image from "next/image";
+import { Loader } from "lucide-react";
+import { Button } from "@app/components/ui/button";
+
 export function Header(): React.JSX.Element {
     return (
         <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-            Header
+            <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                    <Image
+                        src="/mascot.svg"
+                        alt="Duolingo Mascot"
+                        height={40}
+                        width={40}
+                    />
+
+                    <span className="text-2xl font-extrabold text-green-600 tracking-wide">
+                        Duolingo
+                    </span>
+                </div>
+
+                <ClerkLoading>
+                    <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+                </ClerkLoading>
+
+                <ClerkLoaded>
+                    <SignedIn>
+                        <UserButton afterSignOutUrl="/" />
+                    </SignedIn>
+
+                    <SignedOut>
+                        <SignInButton
+                            mode="modal"
+                            afterSignInUrl="/learn"
+                            afterSignUpUrl="/learn"
+                        >
+                            <Button size="lg" variant="ghost">
+                                Login
+                            </Button>
+                        </SignInButton>
+                    </SignedOut>
+                </ClerkLoaded>
+            </div>
         </header>
     );
 }
