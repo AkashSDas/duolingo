534c9d2d63a0d259da241ceb1b43a169fe5c3b16
AkashSDas <aakashdas368@gmail.com>
2024-04-25 23:57:19 +0530

    add userProgress table and relations

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,35 @@
-import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { pgTable, uuid, text, timestamp, integer } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
     id: uuid("id").primaryKey(),
     title: text("title").notNull(),
     imageSrc: text("image_src").notNull(),
     createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const userProgress = pgTable("user_progress", {
+    userId: uuid("user_id").primaryKey(),
+    userName: text("user_name").notNull().default("User"),
+    userImageSrc: text("user_image_src").notNull().default("/mascot.svg"),
+    activeCourseId: uuid("active_course_id").references(() => courses.id, {
+        onDelete: "cascade",
+    }),
+    hearts: integer("hearts").notNull().default(5),
+    points: integer("points").notNull().default(0),
+});
+
+export const coursesRelations = relations(courses, ({ many }) => {
+    return {
+        userProgress: many(userProgress),
+    };
+});
+
+export const userProgressRelations = relations(userProgress, ({ one }) => {
+    return {
+        activeCourse: one(courses, {
+            fields: [userProgress.activeCourseId],
+            references: [courses.id],
+        }),
+    };
 });
