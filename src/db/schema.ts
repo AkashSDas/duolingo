9a68f386340c02028390de91f212e2a8c61f4be7
AkashSDas <aakashdas368@gmail.com>
2024-04-25 23:19:04 +0530

    change course schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,8 @@
-import { pgTable, uuid, text } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
     id: uuid("id").primaryKey(),
     title: text("title").notNull(),
     imageSrc: text("image_src").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
 });
